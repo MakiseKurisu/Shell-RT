f8a7c62d0d3b2d187064f71de023f07259bdb106
牧瀬紅莉栖 <saberconer@gmail.com>
2014-09-07 16:44:18 -0500

    First working demo

--- i386.asm
+++ i386.asm
@@ -1,44 +1,51 @@
 		.386
 		.model flat, stdcall
 		.code
+Address	macro	Register,Tag
+		call	@F
+@@:		pop	Register
+		sub	Register,$-1-Tag
+		endm
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 start:
 		jmp	code_segment
 reloc_segment:
-Kernel32	dd	0	;dd 75e50000
-LoadLibraryA	dd	0	;dd 75e6a669
-GetProcAddress	dd	0	;dd 75e6980c
-
-ExitProcess	dd	0	;dd 75e77f64
+Kernel32	dd	0	;dd 75e50000,75e6a669,75e6980c,75e77f64
+LoadLibraryA	dd	0
+GetProcAddress	dd	0
 
 const_segment:
 User32	db	"User32.dll",0
 MessageBoxA	db	"MessageBoxA",0
+ExitProcess	db	"ExitProcess",0
 
 code_segment:
-		call	@F
-@@:		pop	ecx
-		sub	ecx,$-1-User32
+		Address	ecx,User32
 		push	ecx
-		call	[LoadLibraryA]
+		Address	ecx,LoadLibraryA
+		call	dword ptr [ecx]
 		
-		call	@F
-@@:		pop	ecx
-		sub	ecx,$-1-MessageBoxA
+		Address	ecx,MessageBoxA
 		push	ecx
 		push	eax
-		call	[GetProcAddress]
+		Address	ecx,GetProcAddress
+		call	dword ptr [ecx]
 		
 		push	0
 		push	0
-		call	@F
-@@:		pop	ecx
-		sub	ecx,$-1-MessageBoxA
+		Address	ecx,MessageBoxA
 		push	ecx
 		push	0
 		call	eax
 		
+		Address	ecx,ExitProcess
+		push	ecx
+		Address	ecx,Kernel32
+		push	[ecx]
+		Address	ecx,GetProcAddress
+		call	dword ptr [ecx]
+		
 		push	0
-		call	[ExitProcess]
+		call	eax
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 		end	start
