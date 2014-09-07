828085129803bd463174bc4e732b00d81641aa4e
牧瀬紅莉栖 <saberconer@gmail.com>
2014-09-07 16:04:28 -0500

    This will work in OllyDBG

--- i386.asm
+++ i386.asm
@@ -2,27 +2,43 @@
 		.model flat, stdcall
 		.code
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+start:
 		jmp	code_segment
 reloc_segment:
-Kernel32	dd	0
-LoadLibraryA	dd	0
-GetProcAddress	dd	0
-ExitProcess	dd	0
+Kernel32	dd	0	;dd 75e50000
+LoadLibraryA	dd	0	;dd 75e6a669
+GetProcAddress	dd	0	;dd 75e6980c
+
+ExitProcess	dd	0	;dd 75e77f64
 
 const_segment:
+User32	db	"User32.dll",0
 MessageBoxA	db	"MessageBoxA",0
 
 code_segment:
-		push	offset MessageBoxA
-		push	Kernel32
-		call	GetProcAddress
+		call	@F
+@@:		pop	ecx
+		sub	ecx,$-1-User32
+		push	ecx
+		call	[LoadLibraryA]
+		
+		call	@F
+@@:		pop	ecx
+		sub	ecx,$-1-MessageBoxA
+		push	ecx
+		push	eax
+		call	[GetProcAddress]
 		
 		push	0
 		push	0
-		push	offset MessageBoxA
+		call	@F
+@@:		pop	ecx
+		sub	ecx,$-1-MessageBoxA
+		push	ecx
 		push	0
 		call	eax
 		
-		call	ExitProcess
+		push	0
+		call	[ExitProcess]
 ;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-		end
+		end	start
